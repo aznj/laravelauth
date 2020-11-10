0127648816372ca6d099d583fd7bda928d1791ff
Akmal Jamal <akmal.jamal@photobookworldwide.com>
2020-11-10 14:16:24 +0800

    Protect login page from being accessed again by already logged in user.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store']);
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'store']);
+});
 
 Route::post('/logout', LogoutController::class)->name('logout');
